e29d5cd4c29a6f1425abe04286f1f349ba556bbc
minnow <Gigabytes42>
2020-02-08 21:55:36 -0600

    Main2Activity and textView2

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import android.util.Log;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFunction(View view) {
-        Log.i(TAG, "Button pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myEditText);
+        String str = myTextField.getText().toString();
+        goToActivity2(str);
+        // Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
     }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
+
 }
