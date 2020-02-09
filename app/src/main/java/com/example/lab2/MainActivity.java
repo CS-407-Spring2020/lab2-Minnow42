bebfe16b9d26c21fad202cfebf1a46a384bb67de
minnow <Gigabytes42>
2020-02-08 20:32:27 -0600

    Button log on press

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.util.Log;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "Button Info";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view) {
+        Log.i(TAG, "Button pressed");
     }
 }
